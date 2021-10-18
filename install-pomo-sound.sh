a98571c6e1d49d4405924e67dd941ae4f9314ec5
Pedro Tashima <pedrotashima42@gmail.com>
2021-10-18 13:39:46 -0300

    fix: check if sound already exists before download

--- install-pomo-sound.sh
+++ install-pomo-sound.sh
@@ -6,4 +6,9 @@ curl https://raw.githubusercontent.com/Tashima42/pomo/main/pomo-sound.sh > ~/.lo
 sudo chmod +x ~/.local/bin/pomo
 
 mkdir -p $DOWNLOAD_DIRECTORY 
-wget -P $DOWNLOAD_DIRECTORY https://raw.githubusercontent.com/Tashima42/pomo/main/sound.wav 
+if [ -e $DOWNLOAD_DIRECTORY/sound.wav ]
+then
+    echo "Sound already downloaded"
+else
+    wget -P $DOWNLOAD_DIRECTORY https://raw.githubusercontent.com/Tashima42/pomo/main/sound.wav 
+fi
