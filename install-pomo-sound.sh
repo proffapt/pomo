fe0f9bb4f5342188b0cde8a34a67b0e4991a676d
Pedro Tashima <pedrotashima42@gmail.com>
2021-12-20 10:45:21 -0300

    unified installer

--- install-pomo-sound.sh
+++ install-pomo-sound.sh
@@ -2,13 +2,37 @@
 
 DOWNLOAD_DIRECTORY=~/.config/pomo
 
-curl https://raw.githubusercontent.com/Tashima42/pomo/main/pomo-sound.sh > ~/.local/bin/pomo 
-sudo chmod +x ~/.local/bin/pomo
+function install_with_sound() {
+  echo "Installing with sound notifications"
+  echo "Downloading pomo..."
+  curl https://raw.githubusercontent.com/Tashima42/pomo/main/pomo-sound.sh > ~/.local/bin/pomo && sudo chmod +x ~/.local/bin/pomo
 
-mkdir -p $DOWNLOAD_DIRECTORY 
-if [ -e $DOWNLOAD_DIRECTORY/sound.wav ]
-then
-    echo "Sound already downloaded"
-else
-    wget -P $DOWNLOAD_DIRECTORY https://raw.githubusercontent.com/Tashima42/pomo/main/sound.wav 
-fi
+  echo "Downloading sound to $DOWNLOAD_DIRECTORY" 
+  mkdir -p $DOWNLOAD_DIRECTORY 
+  if [ -e $DOWNLOAD_DIRECTORY/sound.wav ]
+  then
+      echo "Sound already downloaded"
+  else
+      wget -P $DOWNLOAD_DIRECTORY https://raw.githubusercontent.com/Tashima42/pomo/main/sound.wav 
+      echo "Downloaded sound"
+  fi
+}
+
+function install_without_sound() {
+  echo "Installing without sound notifications"
+  echo "Downloading pomo..."
+  curl https://raw.githubusercontent.com/Tashima42/pomo/main/pomo.sh > ~/.local/bin/pomo && sudo chmod +x ~/.local/bin/pomo
+}
+
+while true; do
+  read -p "Install with sound notifications? (y/n)" yn
+  case $yn in
+    [Yy]* )
+      install_with_sound
+      break;;
+    [Nn]* )
+      install_without_sound
+      break;;
+    * ) echo "Please answer yes or no.";;
+    esac
+done
